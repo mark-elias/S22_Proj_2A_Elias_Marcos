ae93bee7405187af2133c085d5d23aab00c0f837
Marcos Elias Cordova <meliascordova@sdsu.edu>
2022-04-22 02:42:05 -0700

    Stage 1 - Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector2 targetPositon;
+
+    public float verticalIncrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPositon, speed * Time.deltaTime);
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            targetPositon = new Vector2(transform.position.x, transform.position.y + verticalIncrement);
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPositon = new Vector2(transform.position.x, transform.position.y - verticalIncrement);
+        }
+    }
+}
